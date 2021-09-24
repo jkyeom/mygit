01428e00e0744ce5f9c05f2cb7af3e9a4ebc916c
jkyeom <qkddnfdl45@naver.com>
2021-09-24 16:58:05 +0900

    테스트!

---
+++ 02_test/src/com/kh/foo/Foo.java
@@ -0,0 +1,10 @@
+package com.kh.foo;
+
+public class Foo {
+	
+	public void Say() {
+		System.out.println("Foo!");
+		System.out.println("Foo2");
+	}
+
+}
---
+++ 02_test/src/com/kh/test/Tester.java
@@ -0,0 +1,13 @@
+package com.kh.test;
+
+import com.kh.foo.Foo;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		new Foo().Say();
+
+	}
+
+}
